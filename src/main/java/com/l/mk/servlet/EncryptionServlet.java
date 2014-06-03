292159de44839817079805d67f380f8391a14c32
xzw <woaishisi@163.com>
2014-06-03 22:18:06 +0800

    初始化Crypher项目

---
+++ src/main/java/com/l/mk/servlet/EncryptionServlet.java
@@ -0,0 +1,49 @@
+package com.l.mk.servlet;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EncryptionServlet
+ */
+public class EncryptionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EncryptionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Enumeration<String> plaintext = request.getParameterNames();
+		while (plaintext.hasMoreElements()) {
+			System.out.println(plaintext.nextElement());
+		}
+		
+		String s = request.getParameter("plaintext");
+		System.out.println(s);
+		
+	
+		//System.out.println(plaintext);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<link type="text/css" href="css/style.css" rel="stylesheet" />
+</head>
+<body>
+	<h1>Hello World!</h1>
+	
+	<form action="/Crypher/EncryptionServlet" method="post">
+		<input type="hidden" name="plaintext" value="name:xzw;tel:18967141232" />
+		<input type="submit" value="Get the Crypher"/>
+	</form>
+	
+</body>
+</html>
