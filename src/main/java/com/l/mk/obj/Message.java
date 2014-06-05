c4c50159faabe98aba6aee885f0077b642c45917
xzw <woaishisi@126.com>
2014-06-05 19:47:23 +0800

    今天修改的东西

--- src/main/java/com/l/mk/obj/Message.java
+++ src/main/java/com/l/mk/obj/Message.java
@@ -1,5 +1,21 @@
 package com.l.mk.obj;
 
 public class Message {
-
+	private MessageHeader messageHeader;
+	private MessageContent messageContent;
+	
+	public MessageHeader getMessageHeader() {
+		return messageHeader;
+	}
+	public void setMessageHeader(MessageHeader messageHeader) {
+		this.messageHeader = messageHeader;
+	}
+	
+	public MessageContent getMessageContent() {
+		return messageContent;
+	}
+	public void setMessageContent(MessageContent messageContent) {
+		this.messageContent = messageContent;
+	}
+	
 }
---
+++ src/main/java/com/l/mk/utils/BCDConverter.java
@@ -0,0 +1,46 @@
+package com.l.mk.utils;
+
+/**
+ * 
+ * @author Administrator
+ * 
+ * BCD码-8421
+ * 将10进制数值进行BCD压缩
+ * 将BCD字节转换十进制字节数组
+ */
+public class BCDConverter {
+	/**
+	 * 对一个字节数组进行BCD压缩，转换成另一个字节数组
+	 * @param src 需要去转换的字节数组
+	 * @return byte[]  转换后的字节数组
+	 */
+	public static  byte[] toBCD(String src) {
+		int length;
+		if (src.length() % 2 == 1) {
+			src = "0" + src;
+		}
+		length = src.length() / 2;
+		byte[] temp = new byte[length];
+		byte[] str = src.getBytes();
+		for (int i=0; i<length; i++) {
+			if (str[2*i] >= '0' && str[2*i] <= '9' &&
+					str[2*i+1] >= '0' && str[2*i+1] <='9') {
+				byte left = (byte)((str[2*i] - '0') << 4);
+				byte right = (byte)(str[2*i+1] - '0');
+				temp[i] = (byte)(left + right);
+			}
+		}
+		
+		return temp;
+	}
+	
+	/**
+	 * 对一个BCD格式的字节数组，转换成正常的字节数组
+	 * @param src BCD编码的字节数组
+	 * @return byte[] 
+	 */
+	public static byte[] fromBCD(byte[] src) {
+		return null;
+	}
+	
+}
