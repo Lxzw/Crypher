9a4d9a2bcb94f360458cc7e197771090e047b94a
mk-win7 <woaishisi@126.com>
2014-06-10 17:30:06 +0800

    将Map对象转换为Message/MessageHeader的操作集合

---
+++ src/main/java/com/l/mk/crypher/format/MessageDataTransfer.java
@@ -0,0 +1,44 @@
+package com.l.mk.crypher.format;
+
+import java.util.Map;
+
+import com.l.mk.crypher.obj.Message;
+import com.l.mk.crypher.obj.MessageHeader;
+
+public class MessageDataTransfer {
+
+	/**
+	 * 将map对象转换为数据报文头
+	 * @param map
+	 * @return
+	 */
+	public static MessageHeader getMessageHeader(Map map) {
+		return null;
+	}
+	
+	/**
+	 * 将map对象的数据转换为Message对象
+	 * @param map
+	 * @return
+	 */
+	public static Message getMessage(Map map) {
+		return null;
+	}
+	
+	/**
+	 * 将MessageHeader对象转换为map
+	 * @param header
+	 * @return
+	 */
+	public static Map messageHeaderToMap(MessageHeader header) {
+		return null;
+	}
+	
+	/**
+	 * 将Message对象转换 Map
+	 */
+	public static Map messageToMap(Message message) {
+		return null;
+	}
+	
+}
