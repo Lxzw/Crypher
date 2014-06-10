aa5c018a3c2af5582855422177ad6a685a0855a5
mk-win7 <woaishisi@126.com>
2014-06-10 17:28:56 +0800

    将XML文件中的数据转换成Map对象

---
+++ src/main/java/com/l/mk/crypher/format/LocalXMLParser.java
@@ -0,0 +1,55 @@
+package com.l.mk.crypher.format;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+public class LocalXMLParser {
+
+	/**
+	 * 從輸入流中獲得XML文件信息，轉換爲map對象
+	 * @param is
+	 * @return
+	 * @throws FileNotFoundException 
+	 */
+	public static Map<String,String> parseMessage(String path) 
+			throws FileNotFoundException {
+		return LocalXMLParser.parseMessage(new File(path));
+	}
+	
+	/**
+	 * 獲得XML文件對象信息，轉換爲map對象
+	 * @param is
+	 * @return
+	 * @throws FileNotFoundException 
+	 */
+	public static Map<String,String> parseMessage(File file) 
+			throws FileNotFoundException {
+		Map<String,String> map = new HashMap<String,String>();
+		Document document = null;
+		try {
+			SAXReader reader = new SAXReader();
+			document = reader.read(file);
+			Element root = document.getRootElement();
+			
+			@SuppressWarnings("unchecked")
+			List<Element> list = root.elements();
+			for (Element e : list) {
+				map.put(e.getName(), e.getTextTrim());
+			}
+			System.out.println(root.getName());
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return map;
+	}
+}
